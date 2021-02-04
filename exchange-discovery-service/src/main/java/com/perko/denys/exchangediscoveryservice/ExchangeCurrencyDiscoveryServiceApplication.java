d1ec31faaddaf4c40bb151ca90bc0bdd2b54cba5
Denys Perko <Denys_Perko@epam.com>
2021-02-04 18:21:13 +0200

    Create Discovery Service

---
+++ exchange-discovery-service/src/main/java/com/perko/denys/exchangediscoveryservice/ExchangeCurrencyDiscoveryServiceApplication.java
@@ -0,0 +1,15 @@
+package com.perko.denys.exchangediscoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ExchangeCurrencyDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeCurrencyDiscoveryServiceApplication.class, args);
+	}
+
+}
---
+++ exchange-discovery-service/src/test/java/com/perko/denys/exchangediscoveryservice/ExchangeCurrencyDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perko.denys.exchangediscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeCurrencyDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
